90f8d1e27709eafc1c8c997571622570f32453b5
Devin Miller <mrpropellers@users.noreply.github.com>
2022-08-15 12:46:51 -0700

    Add mock boss arena; Fix one-way platform collision in player controller

---
+++ BossFightProject/Assets/Scripts/PlayerInputReader.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TarodevController;
+using UnityEngine;
+
+namespace BossFight
+{
+    public class PlayerInputReader : PlayerInput
+    { }
+}
