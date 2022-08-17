d7eacef7270ad9d5b9fd5d511b3ee7435956a1d8
Devin Miller <mrpropellers@users.noreply.github.com>
2022-08-16 18:15:27 -0700

    Add Atoms to project; Wire up some basic game state management

--- BossFightProject/Assets/Scripts/GameplayHelpers.cs
+++ BossFightProject/Assets/Scripts/GameplayHelpers.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
-using LeftOut.GlobalConsts;
 
 namespace BossFight
 {
     public static class GameplayHelpers
     {
-        // TODO: This could traverse all child objects, if that's useful...
-        public static bool BelongsToPlayer(this GameObject self) => self.CompareTag(Tags.Player);
+        /// <summary>
+        /// Checks up the GameObject hierarchy to see if there is any objects in the tree holding a
+        /// PlayerCharacterState component. If there is none - we assume this object doesn't belong to a player
+        /// </summary>
+        public static bool BelongsToPlayer(this GameObject self, out PlayerCharacterStateManager playerCharacterStateManager)
+        {
+            var parent = self.transform.parent;
 
-        public static bool BelongsToPlayer(this Component self) => self.gameObject.BelongsToPlayer();
+            return self.TryGetComponent(out playerCharacterStateManager)
+                || parent != null && BelongsToPlayer(parent.gameObject, out playerCharacterStateManager);
+        }
+
+        public static bool BelongsToPlayer(this GameObject self) => BelongsToPlayer(self, out _);
+
+        public static bool BelongsToPlayer(this Component self) => self.gameObject.BelongsToPlayer(out _);
+
+        public static bool BelongsToPlayer(this Component self, out PlayerCharacterStateManager playerCharacterStateManager) =>
+            BelongsToPlayer(self.gameObject, out playerCharacterStateManager);
     }
 }
